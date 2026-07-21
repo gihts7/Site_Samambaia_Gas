e7fce034c9407bf263dc02fbc43af6fc213727d6
Giih <gihteles502@gmail.com>
2026-07-21 15:45:05 -0300

    Atualizações na tela prinipal

--- www/telaPrincipal.php
+++ www/telaPrincipal.php
@@ -23,48 +23,211 @@
 
 
     <!-- Cabeçalho -->
-
     <header>
+
+    <nav class="navbar navbar-expand-lg">
+
+        <div class="container">
+
+            <a class="navbar-brand" href="#inicio">
+
+                <img src="Imagens/Logo_sem_fundo.png" class="logo-menu">
+
+            </a>
+
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#menu">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="menu">
+
+                <ul class="navbar-nav mx-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#inicio">Início</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#produtos">Produtos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#sobre">Sobre</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contato">Contato</a>
+                    </li>
+
+                </ul>
+
+                <a href="login.php" class="btn btn-login-menu">
+
+                    <i class="bi bi-person-circle"></i>
+
+                    Entrar
+
+                </a>
+
+            </div>
+
+        </div>
+
+    </nav>
 
     </header>
 
 
 
-    <!-- Carrossel -->
+    <!-- Carrossel e história da empresa -->
+    <section id="inicio">
 
-    <section>
+        <div id="carouselPrincipal"
+            class="carousel slide"
+            data-bs-ride="carousel">
 
-    </section>
+        <div class="carousel-inner">
 
+            <div class="carousel-item active">
 
+                <img src="imagens/imgFachada3.jpeg"
+                     class="d-block w-100"
+                     alt="Banner 1">
+
+            </div>
+
+            <div class="carousel-item">
+
+                <img src="imagens/imgFachada2.jpeg"
+                     class="d-block w-100"
+                     alt="Banner 2">
+
+            </div>
+
+            <div class="carousel-item">
+
+                <img src="imagens/imgFachada1.jpeg"
+                     class="d-block w-100"
+                     alt="Banner 3">
+
+            </div>
+
+        </div>
+
+        <button class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselPrincipal"
+                data-bs-slide="prev">
+
+            <span class="carousel-control-prev-icon"></span>
+
+        </button>
+
+        <button class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselPrincipal"
+                data-bs-slide="next">
+
+            <span class="carousel-control-next-icon"></span>
+
+        </button>
+
+    </div>
 
     <!-- Sobre -->
 
-    <section>
+    <div class="sobre">
+
+        <h2>Bem-vindo à Samambaia Gás</h2>
+
+        <p>
+
+            A Samambaia Gás atua na venda de gás de cozinha e água mineral,
+            oferecendo atendimento rápido, segurança e qualidade para
+            Santo Antônio da Patrulha e Caraá.
+
+        </p>
+
+        <a href="#produtos" class="btn-conhecer">
+
+            Conheça nossos produtos
+
+        </a>
+
+    </div>
+
+    <div class="diferenciais">
+
+        <div class="diferencial">
+
+            <i class="bi bi-truck"></i>
+
+            <h4>Entrega rápida</h4>
+
+            <p>
+                Entregamos seu gás com agilidade para que você não fique sem abastecimento.
+            </p>
+
+        </div>
+
+        <div class="diferencial">
+
+            <i class="bi bi-shield-check"></i>
+
+            <h4>Compra segura</h4>
+
+            <p>
+                Atendimento confiável, produtos de qualidade e pagamento com total segurança.
+            </p>
+
+        </div>
+
+        <div class="diferencial">
+
+            <i class="bi bi-geo-alt-fill"></i>
+
+            <h4>Atendimento local</h4>
+
+            <p>
+                Atendemos Santo Antônio da Patrulha e Caraá com rapidez e compromisso.
+            </p>
+
+        </div>
+
+    </div>
 
     </section>
 
 
 
     <!-- Produtos -->
+    <section id="produtos">
 
-    <section>
+        <p>b</p>
 
     </section>
 
 
 
     <!-- Diferenciais -->
+    <section id="infos">
 
-    <section>
+        <p>c</p>
 
     </section>
 
 
 
-    <!-- Rodapé -->
-
+    <!-- Rodapé com infos -->
     <footer>
+
+        <p>d</p>
 
     </footer>
 
