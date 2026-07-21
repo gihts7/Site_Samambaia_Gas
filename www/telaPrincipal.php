7849f0f69f565e84d2f9d983f71bfec9bbdedbaa
Giih <gihteles502@gmail.com>
2026-07-21 14:34:37 -0300

    Início da tela principal na nova branch

--- www/telaPrincipal.php
+++ www/telaPrincipal.php
@@ -1,15 +1,74 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="utf-8">
+
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tela Principal</title>
+
+    <title>Samambaia Gás</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="css/principal.css">
 
 </head>
+
 <body>
 
 
+    <!-- Cabeçalho -->
+
+    <header>
+
+    </header>
+
+
+
+    <!-- Carrossel -->
+
+    <section>
+
+    </section>
+
+
+
+    <!-- Sobre -->
+
+    <section>
+
+    </section>
+
+
+
+    <!-- Produtos -->
+
+    <section>
+
+    </section>
+
+
+
+    <!-- Diferenciais -->
+
+    <section>
+
+    </section>
+
+
+
+    <!-- Rodapé -->
+
+    <footer>
+
+    </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
-</html> 
+</html>
